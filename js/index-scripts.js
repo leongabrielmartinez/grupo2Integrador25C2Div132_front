6ceb6e477a461a710b7fd11ddace28814e2fcbb3
RSS <gabrielmartinezdev@gmail.com>
2025-11-18 10:48:19 -0300

    Estructura inicial refactorizacion [3]

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -1,11 +1,14 @@
 const themeBtn = document.getElementById("theme-toggle");
-const html = document.documentElement;
+// const html = document.documentElement;
 
 function changeColorThemeAttribute(){   
     if (html.getAttribute("data-theme") === "dark") {
+            localStorage.removeItem("data-theme");
             html.removeAttribute("data-theme");
             themeBtn.setAttribute("name", "moon-outline");
+            console.log("here");
         } else {
+            localStorage.setItem("data-theme", "dark");
             html.setAttribute("data-theme", "dark");
             themeBtn.setAttribute("name", "sunny-outline");          
     }}
