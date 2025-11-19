33e8fe3f14da2e5e10e75c18bbca6614aa609d0d
RSS <gabrielmartinezdev@gmail.com>
2025-11-19 08:19:35 -0300

    Estructructura inicial [0]

--- js/global-scripts.js
+++ js/global-scripts.js
@@ -1,8 +1,26 @@
 const html = document.documentElement;
 
+function checkUserTheme(){
 if(localStorage.getItem("data-theme") === "dark"){
     //se activa el modo obscuro, si el usuario tiene en local storage "data-theme" el atributo "dark". 
     //(Que se consigue al presionar el boton de cambio de modo en el formulario inicial).
     html.setAttribute("data-theme", "dark");
+}}
+
+function validateExistNameUser(){
+    if(location.pathname !='/index.html' && location.pathname !='/'){
+    //location.pathname !='/' -> esto es para compatibilidad con go live/el servidor,
+    //lo primero que se abre es / y dsp se redirige automaticamente a index pero en este
+    //proceso se dispara el alert, para evitar eso esta el and.
+        if(!sessionStorage.getItem('userName')){
+            alert("ingrese un nombre");
+            location.replace('index.html')
+        }}
 }
 
+function init(){
+    validateExistNameUser();
+    checkUserTheme();
+}
+
+init()
--- js/index-scripts.js
+++ js/index-scripts.js
@@ -1,17 +1,79 @@
 const themeBtn = document.getElementById("theme-toggle");
 // const html = document.documentElement;
 
-function changeColorThemeAttribute(){   
+function changeColorThemeAttribute(){ 
     if (html.getAttribute("data-theme") === "dark") {
             localStorage.removeItem("data-theme");
             html.removeAttribute("data-theme");
             themeBtn.setAttribute("name", "moon-outline");
-            console.log("here");
         } else {
             localStorage.setItem("data-theme", "dark");
             html.setAttribute("data-theme", "dark");
             themeBtn.setAttribute("name", "sunny-outline");          
     }}
 
+
 themeBtn.addEventListener("click", changeColorThemeAttribute);
+
+let userForm = document.getElementById("user-form");
+
+userForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const userName = document.getElementById('user-name').value;
+
+  let userNameTrimmed = userName.trim();
+
+  if(typeof(userNameTrimmed) === "string" && userNameTrimmed){
+    //Valido que sea un string y que no este vacio.
+    //Segun chat gpt, .value ya define que va a ser un string, pero no estoy seguro...
+    
+    userNameTrimmed = userNameTrimmed.toLocaleLowerCase();
+
+
+    const userNameTrimmedCapitalized = userNameTrimmed
+      .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+
+    //le doy un formato pero no lo valido con regex, podria ser un numero o simbolos. 
+
+    //sessionStorage.removeItem("userName", userNameTrimmedCapitalized)
+
+    sessionStorage.setItem("userName", userNameTrimmedCapitalized);
+    //guardo el nombre el session storage
+   
+    // localStorage.getItem("carrito") -> traerlo para validarlo.
+  }
+}); 
+
+
+// let z = document.getElementById("clearSessionST");
+
+// z.addEventListener("click", () =>{
+//   sessionStorage.removeItem("userName");
+// })
+
+//#region leer este texto.
+//probar de borrar atributo defer de global-scripts.js.
+// //#end region
+
+
+
+//     if (!sessionStorage.getItem('user')){
+//         sessionStorage.setItem('user',
+//           prompt("Ingresa tu nombre"))
+//         location.replace('index.html')
+//     }
+
+
+
+// console.log(location);
+// if(location.pathname !='/index.html'){
+//     if (!sessionStorage.getItem('user')){
+//         // console.log()
+//         prompt("Falta iniciar sesion")
+//         location.replace('index.html')
+//     }
+// }
+
 
